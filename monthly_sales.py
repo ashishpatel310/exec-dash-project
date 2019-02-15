013212e463cb5f296469bc7a066e79b46506ff10
ashishpatel310 <46492064+ashishpatel310@users.noreply.github.com>
2019-02-14 23:13:25 -0500

    add input file selection

--- monthly_sales.py
+++ monthly_sales.py
@@ -4,16 +4,30 @@ import csv
 import itertools
 from operator import itemgetter
 
+def month_lookup(month):
+	year_month={'01':'January','02':'February','03':'March','04':'April',
+	'05':'May','06':'June','07':'July','08':'August','09':'September','10':'October',
+	'11':'November', '12':'December'}
+	return year_month[month]
+
+#Taken and adapted from https://github.com/hiepnguyen034/data_dashboard/blob/master/exec_dash.py
+
+while True:
+	CSV_FILENAME = input('Please enter the time period in the format sales-YYYYMM: ')
+	CSV_NAME = CSV_FILENAME+'.csv'
+	csv_filepath = os.path.join(CSV_NAME)
+	if not os.path.isfile(csv_filepath):
+	 	print("The file does not exist, please make sure to enter a file with correct format, e.g:'sales-201803'")
+	else:
+	 	break
+#Taken and adapted from https://github.com/hiepnguyen034/data_dashboard/blob/master/exec_dash.py
+
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
 
+#csv_filename = "sales-201803.csv" # TODO: allow user to specify
 #
-# INPUTS
-#
-
-csv_filename = "sales-201803.csv" # TODO: allow user to specify
-
-csv_filepath = os.path.join(csv_filename)
+#csv_filepath = os.path.join(csv_filename)
 
 transactions = []
 
@@ -55,6 +69,7 @@ for top_seller in top_sellers:
     product_name = top_seller["name"]
     sales_usd = to_usd(top_seller["monthly_sales"])
     print(f"  {counter}. {product_name} ({sales_usd})")
+
 #Inspired from
 #https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/csv_solution_further.py
 
@@ -65,18 +80,19 @@ for top_seller in top_sellers:
 
 ##Graph Output Setup (Taken from Chart Gallery exercise structure)
 
-import matplotlib.pyplot as plt
-
-bar_data = [sorted_product_sales]
-
-products = []
-sales = []
-
-for d in bar_data:
-  products.append(d["name"])
-  sales.append(d["monthly_sales"])
-
-plt.bar(products, sales)
-plt.ylabel("Sales")
-plt.xlabel("Products")
-plt.show()
+#import matplotlib.pyplot as plt
+#
+#bar_data = [sorted_product_sales]
+#
+#products = []
+#sales = []
+#
+#for d in bar_data: #change d to dict
+#  products.append(d["name"])
+#  sales.append(d["monthly_sales"])
+#
+#plt.bar(products, sales)
+#plt.ylabel("Sales")
+#plt.xlabel("Products")
+#plt.show()
+#
