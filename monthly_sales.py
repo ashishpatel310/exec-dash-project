10628a674daeb9f5eae24cfec68f6b6d4c1ac11b
ashishpatel310 <46492064+ashishpatel310@users.noreply.github.com>
2019-02-14 21:53:07 -0500

    Read file and create list of transactions

--- monthly_sales.py
+++ monthly_sales.py
@@ -14,52 +14,56 @@ csv_filename = "sales-201803.csv" # TODO: allow user to specify
 
 csv_filepath = os.path.join(csv_filename)
 
+transactions = []
 
-#
-# CALCULATIONS
-#
+with open(csv_filepath, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for od in reader:
+        transactions.append(dict(od)) 
+
+print(transactions)
 
 # breakpoint()
 
 #
 # OUTPUTS
 #
-
-print("-----------------------")
-print("MONTH: March 2018") # TODO: get month and year
-
-print("-----------------------")
-print("CRUNCHING THE DATA...")
-
-print("-----------------------")
-print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
-
-print("-----------------------")
-print("TOP SELLING PRODUCTS:")
-
-rank = 1
-for d in top_sellers:
-    print("  " + str(rank) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
-    rank = rank + 1
-
-print("-----------------------")
-print("VISUALIZING THE DATA...")
-
-
-#Graph Output Setup (Taken from Chart Gallery exercise structure)
-
-import matplotlib.pyplot as plt
-
-bar_data = [
-]
-
-products = []
-sales = []
-
-for s in bar_data:
-  products.append(s["products"])
-  sales.append(s["sales"])
-plt.bar(products, sales)
-plt.ylabel("Products")
-plt.xlabel("Sales")
-plt.show()
+#
+#print("-----------------------")
+#print("MONTH: March 2018") # TODO: get month and year
+#
+#print("-----------------------")
+#print("CRUNCHING THE DATA...")
+#
+#print("-----------------------")
+#print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
+#
+#print("-----------------------")
+#print("TOP SELLING PRODUCTS:")
+#
+#rank = 1
+#for d in top_sellers:
+#    print("  " + str(rank) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
+#    rank = rank + 1
+#
+#print("-----------------------")
+#print("VISUALIZING THE DATA...")
+#
+#
+##Graph Output Setup (Taken from Chart Gallery exercise structure)
+#
+#import matplotlib.pyplot as plt
+#
+#bar_data = [
+#]
+#
+#products = []
+#sales = []
+#
+#for s in bar_data:
+#  products.append(s["products"])
+#  sales.append(s["sales"])
+#plt.bar(products, sales)
+#plt.ylabel("Products")
+#plt.xlabel("Sales")
+#plt.show()
