c4ad297274464699be67d45784f321e6b19e4bec
ashishpatel310 <46492064+ashishpatel310@users.noreply.github.com>
2019-02-14 21:45:06 -0500

    Clean up, removing pandas, new imports

--- monthly_sales.py
+++ monthly_sales.py
@@ -1,11 +1,10 @@
-
 import os
-import pandas
+import csv
+import itertools
+from operator import itemgetter
 
-# utility function to convert float or integer to usd-formatted string (for printing)
-# ... adapted from: https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766e9b9ae57a2764725ccc793/shopping_cart.py#L56-L59
 def to_usd(my_price):
-    return "${0:,.2f}".format(my_price) #> $12,000.71
+    return "${0:,.2f}".format(my_price)
 
 #
 # INPUTS
@@ -13,30 +12,13 @@ def to_usd(my_price):
 
 csv_filename = "sales-201803.csv" # TODO: allow user to specify
 
-# reference a file in the "data" directory
-# ... adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md#file-operations
-csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename)
+csv_filepath = os.path.join(csv_filename)
 
-# read csv file into a pandas dataframe object
-# ... this and other pandas operations adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/pandas.md
-csv_data = pandas.read_csv(csv_filepath)
 
 #
 # CALCULATIONS
 #
 
-monthly_total = csv_data["sales price"].sum()
-
-top_sellers = [
-    {"name": "Button-Down Shirt", "monthly_sales": 6960.34},
-    {"name": "Super Soft Hoodie", "monthly_sales": 1875.0},
-    {"name": "Khaki Pants", "monthly_sales": 1602.0},
-    {"name": "Vintage Logo Tee", "monthly_sales": 941.05},
-    {"name": "Brown Boots", "monthly_sales": 250.0},
-    {"name": "Sticker Pack", "monthly_sales": 216.0},
-    {"name": "Baseball Cap", "monthly_sales": 156.31}
-] # TODO: get from CSV data instead
-#
 # breakpoint()
 
 #
@@ -62,9 +44,6 @@ for d in top_sellers:
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
-
-
-
 
 
 #Graph Output Setup (Taken from Chart Gallery exercise structure)
